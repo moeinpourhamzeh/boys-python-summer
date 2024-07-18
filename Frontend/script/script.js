1ca01a0fb753af0cd16902040eff875c40e25d08
Moein <64212565+moeinpourhamzeh@users.noreply.github.com>
2024-07-18 17:33:28 +0330

    session 4

--- Frontend/script/script.js
+++ Frontend/script/script.js
@@ -10,3 +10,19 @@ function clickMe() {
 function showMessage(message) {
     alert(message)
 }
+
+function myArrayCodes() {
+    var myArray = ['ali', 'amir', 'homayoun']
+
+    console.log(myArray)
+    console.log(myArray[0])
+    console.log(myArray[0].toUpperCase())
+
+    for (var i = 0; i < myArray.length; i++) {
+        console.log(myArray[i].toUpperCase())
+    }
+
+    myArray.forEach((item) => {
+        console.log(item.toUpperCase())
+    })
+}

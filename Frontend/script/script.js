29f16b186d6165f5600613ff33690227670eda8d
Moein <64212565+moeinpourhamzeh@users.noreply.github.com>
2024-07-31 16:06:22 +0330

    final Javascript

--- Frontend/script/script.js
+++ Frontend/script/script.js
@@ -19,10 +19,51 @@ function myArrayCodes() {
     console.log(myArray[0].toUpperCase())
 
     for (var i = 0; i < myArray.length; i++) {
-        console.log(myArray[i].toUpperCase())
+        console.log('For Loop ' + myArray[i].toUpperCase())
     }
 
     myArray.forEach((item) => {
-        console.log(item.toUpperCase())
+        console.log('Foreach Loop ' + item.toUpperCase())
     })
+}
+
+// ================ OOP ========================================
+class Student {
+    firstName = ''
+    lastName = ''
+    age = 0
+    constructor(firstName = '', lastName = '', age = 0) {
+        this.firstName = firstName
+        this.lastName = lastName
+        this.age = age
+    }
+}
+
+class Course {
+    title = ''
+    students = []
+    constructor(title = '', students = []) {
+        this.title = title
+        this.students = students
+    }
+}
+
+var ali = new Student('ALi', 'Alinezhad', 16)
+var jafar = new Student('Jafar', 'Jafarnezhad', 20)
+var amir = new Student('Amir', 'Amirzadeh', 18)
+
+var myCourse = new Course('Python Class', [ali, jafar, amir])
+
+function addNewStudent() {
+    var firstName = '' + document.querySelector('#firstName').value
+        // console.log(firstName)
+    var lastName = '' + document.querySelector('#lastName').value
+        // console.log(lastName)
+    var age = parseInt(document.querySelector('#age').value)
+        // console.log(age)
+
+    var newStudent = new Student(firstName, lastName, age)
+
+    myCourse.students.push(newStudent)
+    console.log(myCourse)
 }
